05bfd7e728b5ca509da2f821fad569fddd4df58b
Bruno Gallant <bgallant@bsdnode.net>
2023-05-08 00:06:57 -0400

    placing text and changing attributes

--- sandbox/curses-test.py
+++ sandbox/curses-test.py
@@ -1,9 +1,32 @@
 import curses
 from curses import wrapper
+import time
 
 def main(stdscr):
-    stdscr.clear()
-    stdscr.refresh()
+    curses.init_pair(1, curses.COLOR_RED, curses.COLOR_YELLOW)
+    curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLUE)
+    RED_AND_YELLOW = curses.color_pair(1)
+    GREEN_AND_BLUE = curses.color_pair(2)
+
+    for i in range(100):
+        stdscr.clear()
+        color = RED_AND_YELLOW
+
+        if i % 2 == 0:
+            color = GREEN_AND_BLUE
+        
+        stdscr.addstr(f"Count: {i}", color)
+        stdscr.refresh()
+        time.sleep(0.1)
+
+
+
+    stdscr.addstr(10, 10, "tabarnak", curses.A_BOLD)
+    stdscr.addstr(11, 10, "tabarnak", RED_AND_YELLOW | curses.A_BOLD)
+    stdscr.addstr(12, 10, "tabarnak", GREEN_AND_BLUE | curses.A_BOLD)
+
+
     stdscr.getch()
+
 
 wrapper(main)
