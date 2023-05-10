677fa1c7f2094fa3e4950add91f591cdef50e122
Bruno Gallant <bgallant@bsdnode.net>
2023-05-09 23:14:01 -0400

    nodelay on

--- sandbox/curses-test4.py
+++ sandbox/curses-test4.py
@@ -7,7 +7,7 @@ def main(stdscr):
     curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
     RED_AND_YELLOW = curses.color_pair(1)
     GREEN_AND_BLACK = curses.color_pair(2)
-    # stdscr.nodelay(True)
+    stdscr.nodelay(True)
 
     x, y = 0, 0
     a =0
