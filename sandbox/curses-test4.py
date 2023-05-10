d89d21972fc672e5f350305c2a115500ca145688
Bruno Gallant <bgallant@bsdnode.net>
2023-05-09 23:12:32 -0400

    nodelay concept

--- sandbox/curses-test4.py
+++ sandbox/curses-test4.py
@@ -7,13 +7,34 @@ def main(stdscr):
     curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
     RED_AND_YELLOW = curses.color_pair(1)
     GREEN_AND_BLACK = curses.color_pair(2)
+    # stdscr.nodelay(True)
+
+    x, y = 0, 0
+    a =0
+    while True:
+        try:
+            key = stdscr.getkey()
+        except:
+            key = None
+
+        if key == "KEY_LEFT":
+            x -= 1
+        elif key == "KEY_RIGHT":
+            x += 1
+        elif key == "KEY_UP":
+            y -=1
+        elif key == "KEY_DOWN":
+            y +=1
+    
+        stdscr.clear()
+        a += 1
+        stdscr.addstr(0, a//1000, "tabarnak")
+        stdscr.addstr(curses.LINES -1, 0, f"x: {x}, y: {y}")
+        stdscr.addstr(y, x, "0")
+        stdscr.refresh()
 
 
 
-    key = stdscr.getkey()
 
-    stdscr.addstr(5, 5, f"Key: {key}")
-    stdscr.refresh()
-    stdscr.getch()
 
 wrapper(main)
